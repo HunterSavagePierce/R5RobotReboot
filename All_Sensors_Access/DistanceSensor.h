cd2592168f23a8f3fd996c5af3b106729079a16a
HunterSavagePierce <148508522+HunterSavagePierce@users.noreply.github.com>
2024-04-03 12:31:08 -0500

    Cleaned and Improved DistanceSensor.cpp
    The code compiles however there could be a logical error. I have not checked if the code works with the bot.

--- All_Sensors_Access/DistanceSensor.h
+++ All_Sensors_Access/DistanceSensor.h
@@ -5,16 +5,27 @@
 
 class DistanceSensor {
 public:
-    DistanceSensor(int DATA_PIN);
+    DistanceSensor(int DATA_PIN) {
+        pin = DATA_PIN;
+        pinMode(DATA_PIN, INPUT);
+        for(size_t i = 0; i < ROLLING_AVERAGE_SIZE; ++i) {
+            rolling_average[i] = 0.0; // Initialize rolling window to zeros
+        }
+        sum = 0; // Initialize sum to 0
+    }
+
+    void readSensor();
+    float mapDataToDist(int analog_value);
     float getDistance();
     bool objectFound();
 
 private:
+    static const size_t ROLLING_AVERAGE_SIZE = 20;
     int pin;
     int analog_distance;
-
-    void readSensor();
-    float mapDataToDist(int analog_value);
+    float rolling_average[ROLLING_AVERAGE_SIZE] = {};
+    size_t arr_i = 0;
+    float sum = 0; // Member variable for the running sum
 };
 
 #endif
